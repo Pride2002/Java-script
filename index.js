32b21e5142d04377a13a6eb1903ad54219b65599
Pride2002 <malenpride02@gmail.com>
2026-03-25 08:33:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("My name is Pride");
+
+// Variables
+// Declaring variables uses the var, let, or const keywords
+
+// var is a function-scoped and can be redeclared and updated
+var name = "Pride";
+console.log(name)
+name = "Malen Pride";
+
+// let is block-scoped and can be updated but not redeclared
+let age = 25;
+console.log(age);
+age = 18;
+console.log("My age has been changed because am just a girl",age);
+
